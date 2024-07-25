b6c78c9ae8972f0b83260447f47eb3166044687a
Pete Peterson <petersonpf@ornl.gov>
2024-07-25 10:57:24 -0400

    First plot

--- mon_outage.py
+++ mon_outage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+import json
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import json
 
 ######################################################################
 # This is focused on the period from just before to just after
@@ -24,6 +25,32 @@ runs_save = runs_save.rename(
     }
 )
 runs_save["endtime"] = pd.to_datetime(runs_save["endtime"])
+runs_save.duration /= 3600.0
+
+instruments = np.unique(runs_save.instr)
+
+fig = plt.figure(layout="constrained")
+fig.subplots_adjust(wspace=0)
+ax = fig.subplots(2, 1, sharex="col")
+fig.tight_layout()
+for instr in instruments:
+    mask = runs_save.instr == instr
+    if np.count_nonzero(mask) < 15:  # only show things with a minimum number of runs
+        continue
+    ax[0].scatter(runs_save.endtime[mask], runs_save.duration[mask], label=instr)
+ax[0].set_ylabel("acquisition (in hour)")
+ax[0].legend()
+
+ax[1].set_xlabel("time-of-day")
+
+
+time_range = runs_save.endtime[runs_save.endtime.size - 1] - runs_save.endtime[0]
+for instr in instruments:
+    mask = runs_save.instr == instr
+    number = np.count_nonzero(mask)
+    print(f"{instr:7} - {number:4d} runs - 1 run every " + str(time_range / number))
+print("Overall 1 run every " + str(time_range / runs_save.size))
+print(f"    observed over {time_range} for {runs_save.size}")
 
 ##### load list of AR restarts
 ar_restarts = {}
@@ -43,25 +70,35 @@ with open("AR_restarts", "r") as handle:
             pass  # don't bother with the other lines
 
 
+CAT_STARTED = "/queue/CATALOG.ONCAT.STARTED:"
+REDUX_STARTED = "/queue/REDUCTION.STARTED:"
+REDUX_DISABLED = "/queue/REDUCTION.DISABLED:"
+REDUX_CAT_STARTED = "/queue/REDUCTION_CATALOG.STARTED:"
+REDUX_CREATE_SCRIPT = "/queue/REDUCTION.CREATE_SCRIPT:"
+
+
 def loadPostProcLog(filename):
     def hasRunID(msg):
         if "Input queues:" in msg:
             return False
 
+        if REDUX_CREATE_SCRIPT in msg:
+            return False  # not going to both with these
+
         queues = [
             "/queue/CATALOG.ONCAT.DATA_READY:",
-            "/queue/CATALOG.ONCAT.STARTED:",
+            CAT_STARTED,
             "/queue/CATALOG.ONCAT.COMPLETE:",
             "/queue/REDUCTION.DATA_READY:",
-            "/queue/REDUCTION.STARTED:",
+            REDUX_STARTED,
             "/queue/REDUCTION.COMPLETE:",
-            "/queue/REDUCTION.DISABLED:",
+            REDUX_DISABLED,
             "/queue/REDUCTION_CATALOG.DATA_READY",
-            "/queue/REDUCTION_CATALOG.STARTED",
+            REDUX_CAT_STARTED,
             "/queue/REDUCTION_CATALOG.COMPLETE",
         ]
         for queue in queues:
-            if queue and queue in msg:
+            if queue in msg:
                 return True
         return False
 
@@ -69,19 +106,20 @@ def loadPostProcLog(filename):
         # find the just with the run info
         try:
             start = msg.index("{")
-            stop = msg.index("}")
+            stop = msg.rindex("}")
             msg = msg[start : stop + 1]
         except ValueError as e:
             raise RuntimeError(msg) from e
 
-        # load into json
         try:
+            # load into json
             stuff = json.loads(msg)
-        except json.JSONDecodeError as e:
+
+            # reduce the text and return
+            msg = f"{stuff['facility']} {stuff['instrument']} {stuff['ipts']} {stuff['run_number']}"
+        except Exception as e:
             raise RuntimeError(msg) from e
 
-        # reduce the text and return
-        msg = f"{stuff['facility']} {stuff['instrument']} {stuff['ipts']} {stuff['run_number']}"
         return msg
 
     def cleanMsg(msg):
@@ -105,9 +143,15 @@ def loadPostProcLog(filename):
 
         # shorten commands
         if msg.startswith("WARN Command"):
-            msg = (
-                "WARN PostProcessAdmin.py -q" + msg.split(",")[3] + " " + getRunID(msg)
-            )
+            if hasRunID(msg):
+                msg = (
+                    "WARN PostProcessAdmin.py -q"
+                    + msg.split(",")[3]
+                    + " "
+                    + getRunID(msg)
+                )
+            else:
+                pass
         elif msg.startswith("INFO Created thread"):
             msg = "INFO Created thread to start daemon"
         elif msg.startswith("INFO Starting receiver loop"):
@@ -130,11 +174,38 @@ def loadPostProcLog(filename):
                 messages.append(cleanMsg(message))
 
     timestamps = pd.to_datetime(np.asarray(timestamps), format=r"%Y-%m-%d %H:%M:%S")
-    for stmp, msg in zip(timestamps, messages):
-        if True:  # msg.startswith("WARN"):
-            print(stmp, msg)
+    # for stmp, msg in zip(timestamps, messages):
+    #    if True:  # msg.startswith("WARN"):
+    #        print(stmp, msg)
     print(f"THERE ARE {len(timestamps)} messages")
-    return timestamps, messages
+    # df = pd.DataFrame((timestamps, messages), columns=["time", "message"])
+    df = pd.DataFrame({"time": timestamps, "message": messages})
+    return df
 
 
-stuff = loadPostProcLog("AR1_postprocessing.log")
+for AR_NUM in (1, 2, 4):
+    stuff = loadPostProcLog(f"AR{AR_NUM}_postprocessing.log")
+
+    mask = stuff.message == "HEARTBEAT"
+    ax[1].scatter(
+        stuff.time[mask], np.zeros(np.count_nonzero(mask), dtype=float) + AR_NUM
+    )
+
+    for keyword, offset in (
+        (CAT_STARTED, 0.1),
+        (REDUX_STARTED, 0.2),
+        (REDUX_DISABLED, 0.2),
+        (REDUX_CAT_STARTED, 0.3),
+    ):
+        mask = np.zeros(stuff.message.size, dtype=bool)
+        for i, msg in enumerate(stuff.message):
+            mask[i] = bool("/queue/REDUCTION.STARTED: " in msg)
+        ax[1].scatter(
+            stuff.time[mask],
+            np.zeros(np.count_nonzero(mask), dtype=float) + (AR_NUM + offset),
+        )
+
+for axis in ax:
+    axis.label_outer()
+
+fig.show()
